76cd1c0fb68318c75fceb9c19e5cb5940491f793
清水友裕 <live.at.my.own.pace.9@gmail.com>
2019-11-30 13:38:26 +0900

    first commit

---
+++ .zlogin
@@ -0,0 +1,1 @@
+/Users/tomohiro/.zprezto/runcoms/zlogin
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: "getDirectoryPath" && {
+		#　本ファイル が存在するカレントディレクトリの絶対パスをカレントディレクトリを移動せずに取得
+		cwd=`dirname "${0}"`
+		DOTFILES_PATH=`(cd "${cwd}" && pwd)`
+}
+
+: "mkSymboliclink" && {
+
+		#　正規表現で設定ファイルを取得し、シンボリックリンクを貼る
+		for f in .??*
+		do
+				#　除外リスト
+				[[ "$f" == ".git" ]] && continue
+				[[ "$f" == ".gitignore" ]] && continue
+				[[ "$f" == ".DS_Store" ]] && continue
+
+				#　シンボリックリンクを貼る
+				ln -snfv ${DOTFILES_PATH}/"${f}" ~/"${f}"
+		done
+}
