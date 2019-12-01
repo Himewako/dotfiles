079b1a71516560ca1c95ad1d98ea843f0a2a25c0
清水友裕 <live.at.my.own.pace.9@gmail.com>
2019-12-01 15:21:30 +0900

    プロンプトの整備

---
+++ .configFiles/zsh/status_code.zsh
@@ -0,0 +1,9 @@
+status_code () {
+		local color face
+		
+		color="%(?.${fg[green]}.${fg[blue]})"
+		face="%(?!(｡╹ω╹｡)何する〜? < !(ｏдｏlll%)できんかった < )"
+#		face="%(?!(*'-') < !(*;-;%)? < )"
+
+		echo "${color}${face}${reset_color}"
+}
